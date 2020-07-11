06f8dba8fd9dbbdbd65a0383b8945dc44a28c9e3
FlippieCoetser <flippie.coetser@gmail.com>
2020-07-11 21:49:51 +0200

    Rename and Refactor MCP23018 Tests

--- MCP23018.Test.py
+++ MCP23018.Test.py
@@ -5,31 +5,32 @@ import os
 
 # Import all required dependecies
 import sys
-from MCP2221_A import I2C, DigitalInOut, Direction, G0
+from MCP2221_1 import I2C, Pin
 
 import time
 time.sleep(1)
 
+# Import the MCP23018 Library and ENUMS
+from MCP23018 import MCP23018 , PORT, PIN, PARAMETERS, PARAMETER
+from ENUMS import DIRECTION, STATE
+
 # Configure MCP2221 pin G0 as output. 
 # G0 is connected to the reset pin of the MCP23018
-pin = DigitalInOut(G0)
-pin.direction = Direction.OUTPUT
-
-# Import the MCP23018 Library and ENUMS
-from MCP23018 import MCP23018 , PORT, PIN, STATE, DIRECTION, PARAMETERS, PARAMETER
+pin = Pin(0)
+pin.direction = DIRECTION.OUT
 
 # Load the G0 pin and the i2c interface into MCP2308
 mcp23018 = MCP23018(RESET_Pin = pin, I2C_Interface=I2C)
 
+# Debug Flags
+mcp23018.RESET.debug = True
+mcp23018.I2C.debug = False
+mcp23018.Configuration.debug = False
+mcp23018.GPIO.port[PORT.B].pin[PIN.GP5].debug = False
+
 # Initialize
 mcp23018.reset()
 mcp23018.wait(1)
-
-# Debug Flags
-mcp23018.RESET.debug = True
-mcp23018.I2C.debug = True
-mcp23018.Configuration.debug = True
-mcp23018.GPIO.port[PORT.B].pin[PIN.GP5].debug = True
 
 #Configure GPIO at OUTPUT
 mcp23018.GPIO.value = STATE.HIGH
